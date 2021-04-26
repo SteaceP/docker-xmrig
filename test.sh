14638f438a0312d46c1e369e913edc1d3b3051f9
Steacy Paquette <3757251+SteaceP@users.noreply.github.com>
2021-04-26 15:25:40 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@
 docker stop xmrig
 docker rm xmrig
 docker build . -t xmrig
-docker run -d -e TZ="Australia/Melbourne" -p 127.0.0.1:80:80/tcp --name="xmrig" xmrig
+docker run -d -e TZ="Canada/Toronto" -p 127.0.0.1:80:80/tcp --name="xmrig" xmrig
 docker logs xmrig -f
